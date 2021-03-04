2f692da6eb39ed4b5e68490fa218cc020058736f
ManuelGF <manuelg139@gmail.com>
2021-03-04 02:50:13 -0600

    Added the About Me Style andfixed navigation

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,13 @@
-
-
-
 $( document ).ready(function() {
+    // Toogling the menu down
     $('.menu-toggler').on('click', function(){
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
+    });
+
+    // Toogling the menu up
+    $('.top-nav .nav-link').on('click', function(){
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
     });
 });
